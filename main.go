19893626d0025853694bcdcc8cc53c5f31e169d0
zexxy90fps <bagusandika2108@gmail.com>
2024-07-25 12:46:50 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import "fmt"
+
+func main() {
+	var num1, num2 int
+	var operator string
+
+	//meminta input dari pengguna
+	fmt.Println("masukan angka pertama: ")
+	_, err1 := fmt.Scanln(&num1)
+	if err1 != nil {
+		fmt.Println("input tidak valid: ", err1)
+		return
+	}
+	fmt.Println("masukan operator (+, -, *, /):")
+	_, err2 := fmt.Scanln(&operator)
+	if err2 != nil || (operator != "+" && operator != "-" && operator != "*" && operator != "/") {
+		fmt.Println("operator tidak valid: ", err2)
+		return
+	}
+
+	fmt.Println("masukan angka kedua:")
+	_, err3 := fmt.Scanln(&num2)
+	if err3 != nil {
+		fmt.Println("input tidak valid: ", err3)
+		return
+	}
+
+	//melakukan operasi yang dipilih
+	var result int
+	switch operator {
+	case "+":
+		result = num1 + num2
+	case "-":
+		result = num1 - num2
+	case "*":
+		result = num1 * num2
+	case "/":
+		if num2 == 0 {
+			fmt.Println("error : kesalahan:pembagian nol tidak diperbolehkan.")
+			return
+		}
+		result = num1 / num2
+	}
+
+	//menampilkan hasil
+	fmt.Printf("Ini lah hasilnya: %d", result)
+	fmt.Printf("")
+
+}
